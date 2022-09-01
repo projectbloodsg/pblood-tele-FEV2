6f3cad1909d7e0d85b2c6327f7bf6991d9d6aba9
jensenhuangyankai <jensen@huangyankai.com>
2022-09-01 17:26:10 +0800

    GOTTEN v1 OF CMS WORKING

--- app.py
+++ app.py
@@ -34,32 +34,84 @@ def getChild(id):
 
     return response.json()
 
-@app.route("/") #this route is the most basic view
+def newChildMessage(data):
+    response = requests.post("{base_url}/messages".format(base_url=BASE_URL),data)
+    if response.status_code != 201:
+        print("Result not found!")
+    return response.json()
+
+def updateMessage(id,data):
+    response = requests.put("{base_url}/messages/id/{messageId}".format(base_url=BASE_URL, messageId=id),data)
+    if response.status_code != 200:
+        print("Result not found!")
+    return response.json()
+
+def deleteMessage(id):
+    response = requests.delete("{base_url}/messages/id/{messageId}".format(base_url=BASE_URL, messageId=id))
+    if response.status_code != 204:
+        print("Result not found!")
+    return response.json()
+
+
+
+
+@app.route("/", methods=['GET','POST']) #this route is the most basic view
 def index():
-    parent = {}
-    children = []
-    try:
-        parent = getFirst()[0]
-        parent_content = parent['content']
-        children = getChild(parent['message_id'])
-        
-    except Exception as e:
-        print(e)
-    return render_template("index.html",parent = parent, children = children)
+    if request.method == "GET":
+        parent = {}
+        children = []
+        try:
+            parent = getFirst()[0]
+            #ic(parent)
+            children = getChild(parent['message_id'])
+            
+        except Exception as e:
+            print(e)
+        return render_template("index.html",parent = parent, children = children, root = True)
 
 
-# you can also use a particular data type such as int,str
-# @app.route('post/<int:id>', methods=['GET', 'POST'])
-@app.route('/<string:id>', methods=['GET'])
+""" you can also use a particular data type such as int,str
+    @app.route('post/<int:id>', methods=['GET', 'POST'])"""
+@app.route('/<string:id>', methods=['GET','POST'])
 def render_new_parent(id):
-    ic(id)
-    parent = {}
-    children = []
-    try:
-        parent = getMessage(id)
-        ic(parent)
-        children = getChild(parent['message_id'])
+    if request.method == "GET":
+        #ic(id)
+        parent = {}
+        children = []
+        try:
+            parent = getMessage(id)
+            #ic(parent)
+            children = getChild(parent['message_id'])
+            
+        except Exception as e:
+            print(e)
+        return render_template("index.html",parent = parent, children = children)
+    if request.method == "POST":
+        """it will only receive POST if i press "done", and it will update the database from here with the changed content"""
+        from_js = request.json #data from javascript
+
+        ic(from_js)
+        """EDITING OF MESSAGES"""
+        if from_js['type'] == 'edit_message':
+            edited_message = from_js['message']
+            data = {"content": edited_message,
+                "image_url": "",}
+            updateMessage(id,data) ####REMEMBER TO ENABLE THIS LATER
         
-    except Exception as e:
-        print(e)
-    return render_template("index.html",parent = parent, children = children)
+
+        """CREATION OF CHILD MESSAGES"""
+        if from_js['type'] == 'new_child_message':
+            data = {
+                "content": from_js['message'],
+                "image_url": "",
+                "parent_id": from_js['parent_id'],
+            }
+
+            newChildMessage(data) ####REMEMBER TO ENABLE THIS LATER
+        
+        """DELETION OF MESSAGE"""
+        if from_js['type'] == 'delete_message':
+            ic(deleteMessage(from_js['id']))
+
+        return redirect(request.url)
+        
--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,61 @@
+window.post = function(url, data) {
+  return fetch(url, {method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
+} //function to post 
 
 function start_editing(elem,count){
   edit = "edit-" + count;
-  console.log(edit)
   paragraph = document.getElementById(edit);
   paragraph.parentNode.style.pointerEvents = "none";
   paragraph.contentEditable = true;
   paragraph.style.backgroundColor = "#dddbdb";
+  end_editing_button_id = "end-editing-" + count;
+  end_editing_button = document.getElementById(end_editing_button_id);
+  end_editing_button.hidden = false;
 }
 
-function end_editing(elem,count){
+function end_editing(elem,count,message_id){
   edit = "edit-" + count;
-  console.log(edit)
   paragraph = document.getElementById(edit);
   paragraph.parentNode.style.pointerEvents = "";
   paragraph.contentEditable = false;
   paragraph.style.backgroundColor = "#ffe44d";
+  console.log(paragraph.textContent);
+  path = "/" + message_id;
+  post(path, {'type': 'edit_message', 'message': paragraph.textContent}); //posts edited message back to the python app
+  end_editing_button_id = "end-editing-" + count;
+  end_editing_button = document.getElementById(end_editing_button_id);
+  end_editing_button.hidden = true;
+}
+
+function start_child_message(){
+  message = document.getElementById('create-child-message');
+  if (message.hidden == true){
+    message.hidden = false;
+  }
+  else if (message.hidden == false){
+    message.hidden = true;
+  }
+}
+
+function submit_child_message(parent_id){
+  message = document.getElementById('freeform');
+  /*console.log(message.value)*/
+  path = "/" + parent_id;
+  console.log(path)
+  console.log({'type': 'new_child_message', 'message': message.value, 'parent_id' : parent_id})
+  post(path, {'type': 'new_child_message', 'message': message.value, 'parent_id' : parent_id}); //posts new child message back to the python app
+  start_child_message()
+}
+
+function delete_message_confirm(count,message_id){
+  col_wrapper_number = "col wrapper " + count;
+  col_wrapper = document.getElementById(col_wrapper_number);
+  path = "/" + message_id;
+  if (confirm("Are you sure you want to delete this child message?")) {
+    post(path, {'type': 'delete_message', 'id': message_id});
+    col_wrapper.hidden = true;
+  }
+  
 }
 
 
